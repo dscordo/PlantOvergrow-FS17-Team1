bcdf8c426b5d3ccd15e708834f90a3fbc9fbdd44
Cristina Fernández <cris.fernandez.alvarez@gmail.com>
2022-03-15 13:09:29 +0100

    backend routes working, db tables connected

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require("morgan");
 const cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var plantinfoRouter = require("./routes/plantinfo");
+var wishlistRouter = require("./routes/wishlist");
 
 var app = express();
 
@@ -16,5 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/plantinfo", plantinfoRouter);
+app.use("/wishlist", wishlistRouter);
 
 module.exports = app;
--- model/plantovergrow.sql
+++ model/plantovergrow.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE if exists users;
 CREATE TABLE `users`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(100) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
     `password` VARCHAR(255) NOT NULL
@@ -10,7 +10,7 @@ CREATE TABLE `users`(
 
 DROP TABLE if exists plantinfo;
 CREATE TABLE `plantinfo`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `userid` INT NOT NULL,
     `pid` VARCHAR(100) NOT NULL,
     `lastwater` DATE NULL,
@@ -19,24 +19,25 @@ CREATE TABLE `plantinfo`(
     `notes` VARCHAR(255) NULL,
     `userimage` VARCHAR(255) NULL,
     `startdate` DATE NULL,
-    PRIMARY KEY (id,userid)
+    PRIMARY KEY (id,userid),
+    FOREIGN KEY (userid) REFERENCES users(id)
 );
 
 
 DROP TABLE if exists wishlist;
 CREATE TABLE `wishlist`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `userid` INT NOT NULL,
     `pid` VARCHAR(100) NOT NULL,
     `notes` VARCHAR(255) NULL,
-    PRIMARY KEY (id,userid)
+    PRIMARY KEY (id,userid),
+    FOREIGN KEY (userid) REFERENCES users(id)
 );
 
 
-/* INSERT INTO `users`(`id`, `name`, `email`, `password`) VALUES (1000, "Maria", "maria@gmail.com", "1234");
- */
-/* INSERT INTO `plantinfo`(`id`, `userid`, `pid`, `lastwater`, `lastfert`, `lastrepot`, `notes`, `userimage`, `startdate`) 
+INSERT INTO `users`(`id`, `name`, `email`, `password`) VALUES (1000, "Maria", "maria@gmail.com", "1234");
+
+INSERT INTO `plantinfo`(`id`, `userid`, `pid`, `lastwater`, `lastfert`, `lastrepot`, `notes`, `userimage`, `startdate`) 
 VALUES (111, 1000, "cocos nucifera", "2022-03-03", "2022-02-02", "2020-08-08", "coco coquito coquero", "fotoquenotengo.jpg", "2018-10-10");
- */
-/* INSERT INTO `wishlist`(`id`, `userid`, `pid`, `notes`) VALUES (222, 1000, "alocasia cucullata", "oreja de elefante enano encapuchado");
- */
+
+INSERT INTO `wishlist`(`id`, `userid`, `pid`, `notes`) VALUES (222, 1000, "alocasia cucullata", "oreja de elefante enano encapuchado");
