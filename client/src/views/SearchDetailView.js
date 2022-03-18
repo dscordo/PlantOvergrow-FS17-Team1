4f595adbfc4ebe1ef38f5bc242c837a1499a4dd1
Lau-cm <laura.capelm@gmail.com>
2022-03-18 17:14:37 +0100

    API FUNCIONA

--- client/src/views/SearchDetailView.js
+++ client/src/views/SearchDetailView.js
@@ -1,45 +1,21 @@
 import React, { useState } from "react";
-import ExternalApi from "../helpers/ExternalApi";
+import { Link } from "react-router-dom";
 
-export default function SearchDetailView() {
-  // const [detail, setDetail] = useState("");
 
-  // const handleChange = (e) => {
-  //   const { value } = e.target;
 
-  //   setDetail(value);
-  //   console.log(e.target.value, "value");
-  // };
+export default function SearchDetailView(props) {
 
-  // const handleSubmit = (e) => {
-  //   //sends item to App through props.addItem
-  //   e.preventDefault();
-  //   showDetails(detail);
-  //   console.log("handlesubmit", detail);
-  // };
-
-  // async function showDetails(pid) {
-  //   console.log("showDetails", pid);
-  //   let response = await ExternalApi.getPlantDetail(pid);
-  //   console.log("showDetails", ExternalApi.getPlantDetail(pid));
-  //   if (response.ok) {
-  //     setDetail(response.data);
-  //   } else {
-  //     setDetail(null);
-  //   }
-  // }
-
-//   return (
-//     <div className="searchDetail">
-//       SearchDetailView
-//       <ul>
-//         {props.x.map((i) => (
-//           <li key={i.pid}>
-            // <link to={"/PlantCard/"+i.pid}>{i.pid}</link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
+  return (
+    <div className="searchDetail">
+      
+      <ul>
+        {props.plantResults.map((i) => (
+          <li key={i.pid}>
+            <Link to={"/PlantCard/"+i.pid}>{i.pid}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
